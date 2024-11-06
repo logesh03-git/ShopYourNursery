1fa254a5c182b4616eec8c4e5c080d4f29b992b6
Ajay <ajayreigns0@gmail.com>
2024-11-06 10:20:10 +0530

    fix: serving static files

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,7 @@ const port = 3001;
 app.use(express.json());
 app.use(cors());
 
-// app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 app.use("/api", apiRouter);
 
