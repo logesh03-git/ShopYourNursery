d10f2be4968a63aa343d3fa2c5eee5dc1f8eb775
Ajay <ajayreigns0@gmail.com>
2024-11-14 16:28:16 +0530

    api: getting plants

--- server/src/routes/plants.route.js
+++ server/src/routes/plants.route.js
@@ -13,13 +13,12 @@ router.post("/", async (req, res) => {
   }
 });
 router.get("/", async (req, res) => {
-  res.send("getting plants");
-  // try {
-  //   const plants = await Plant.find({});
-  //   res.json(plants);
-  // } catch (error) {
-  //   return res.status(500).json({ message: "Error while fetching plants" });
-  // }
+  try {
+    const plants = await Plant.find({});
+    res.json(plants);
+  } catch (error) {
+    return res.status(500).json({ message: "Error while fetching plants" });
+  }
 });
 router.get("/:id", async (req, res) => {
   const id = req.params.id.toString();
